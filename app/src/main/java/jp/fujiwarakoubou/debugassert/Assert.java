24a1172bf839e6abf825ad04df4f1befdd11f872
glasses_regia <glasses.regia@gmail.com>
2015-07-23 00:29:18 +0900

    initialize

---
+++ app/src/main/java/jp/fujiwarakoubou/debugassert/Assert.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 Takahito Fujiwara
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+package jp.fujiwarakoubou.debugassert;
+
+/**
+ * Created by Takahito Fujiwara on 2015/07/23.
+ */
+public class Assert {
+}
