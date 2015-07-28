a5c662f87f8b2ed1054bee53b3f90612048c8e8f
glasses_regia <glasses.regia@gmail.com>
2015-07-28 18:34:48 +0900

    とりあえず必要そうな実装

--- library/src/main/java/jp/fujiwara/koubou/debugassert/Assert.java
+++ library/src/main/java/jp/fujiwara/koubou/debugassert/Assert.java
@@ -6,13 +6,100 @@
 
 package jp.fujiwara.koubou.debugassert;
 
-import android.app.Activity;
+import android.app.FragmentManager;
+import android.util.Log;
 
 /**
  * Created by Takahito Fujiwara on 2015/07/23.
  */
 public class Assert {
-    public static void assertTest(Activity activity) {
-        AssertDialog.showDialog(activity, "test");
+    private static final String TAG = "Assert";
+    private static final Assert ins = new Assert();
+    private FragmentManager manager;// staticにしてしまうのも考えたが、メモリリークが怖かったのでシングルトンにして参照（チキン）
+
+    private Assert(){}
+    public static Assert of() {
+        return  ins;
+    }
+
+    /**
+     * DialogFragmentに必要なFragmentManagerを指定する
+     * @param manager
+     */
+    public void setFragmentManager(FragmentManager manager) {
+        this.manager = manager;
+
+        // 実のところ、Assert専用のActivityを作り、そこからFragmentManagerを取得する形にした方がよい。
+        // しかしその場合は、ライブラリプロジェクトなので、このプロジェクト内には<activity>を宣言することができない。
+        // つまり、このライブラリを使用する側に宣言する必要があるため、このライブラリを拡張しようとする別のライブラリに問題を与えてしまう。
+        // よって拡張性を残すために必要に応じてFragmentManagerを指定するようにしてある。
+    }
+
+    private void showDialog(String message) {
+        Log.d(TAG, message);
+        if (manager == null) {
+            Log.w(TAG, "FragmentManager of Assert is null. #setFragmentmanager is requiered.");
+            return;
+        }
+        new AssertDialog().showDialog(manager, "test");
+    }
+
+    /**
+     * 「test」が表示されるアサート
+     */
+    public void assertTest() {
+        showDialog("test");
+    }
+
+    /**
+     * message をアサート
+     * @param message
+     */
+    public void assertMessage(String message) {
+        showDialog(message);
+    }
+
+    /**
+     * object が null なら message をアサート
+     * @param message
+     * @param object
+     */
+    public void assertNull(String message, Object object) {
+        if (object == null) {
+            showDialog(message);
+        }
+    }
+
+    /**
+     * object が not null なら message をアサート
+     * @param message
+     * @param object
+     */
+    public void assertNotNull(String message, Object object) {
+        if (object != null) {
+            showDialog(message);
+        }
+    }
+
+    /**
+     * judge が true なら message をアサート
+     * @param message
+     * @param judge
+     */
+    public void assertTrue(String message, boolean judge) {
+        if (judge == true) {
+            showDialog(message);
+        }
+    }
+
+    /**
+     * judge が false なら message をアサート
+     * @param message
+     * @param judge
+     */
+    public void assertFalse(String message, boolean judge) {
+        if (judge == false) {
+            showDialog(message);
+        }
     }
 }
--- library/src/main/java/jp/fujiwara/koubou/debugassert/AssertDialog.java
+++ library/src/main/java/jp/fujiwara/koubou/debugassert/AssertDialog.java
@@ -6,21 +6,21 @@
 
 package jp.fujiwara.koubou.debugassert;
 
-import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 
 /**
  * Created by Takahito Fujiwara on 2015/07/25.
  */
 public class AssertDialog {
-    public static void showDialog(Activity activity, String message) {
+    public void showDialog(FragmentManager manager, String message) {
         // DialogFragment.show() will take care of adding the fragment
         // in a transaction.  We also want to remove any currently showing
         // dialog, so make our own transaction and take care of that here.
-        FragmentTransaction ft = activity.getFragmentManager().beginTransaction();
-        Fragment prev = activity.getFragmentManager().findFragmentByTag("dialog");
+        FragmentTransaction ft = manager.beginTransaction();
+        Fragment prev = manager.findFragmentByTag("dialog");
         if (prev != null) {
             ft.remove(prev);
         }
